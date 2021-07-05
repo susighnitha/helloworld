b39fc1a972549e0a4657a73755fdc54559a85344
susighnitha <85884902+susighnitha@users.noreply.github.com>
2021-07-05 22:16:16 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,10 @@
 public class HelloWorld { 
  
 	public static void main(String[] args) {
-			  System.out.println("HelloWorld");
+
+	 System.out.println("HelloWorld");
+		System.out.println("modified code");
 }
+
+
 }
